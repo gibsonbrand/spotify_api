4410fc192035da2dcf7e9631f69569242ead8c61
gibsonbrand <gibsonbrand@gmail.com>
2021-12-11 15:41:38 -0600

    test

--- login.js
+++ login.js
@@ -3,7 +3,7 @@ var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
     if ( username == "test" && password == "pass"){
         alert ("Login successfully");
-        window.location = "genre.html"; 
+        window.location = "popular.html"; 
     return false;
     }
     else{
